95251166913aa03aa2e1e3d47bc2769862d671c7
Krzysztof Augustyniak <augustyniak@o2.pl>
2020-10-31 19:25:53 +0100

    JS changes 5-3-7

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!'); 
+  console.log('Link was clicked!');
   
   /* Done remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -10,12 +11,12 @@ function titleClickHandler(event){
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* Done add class 'active' to the clicked link */
 
   clickedElement.classList.add('active')
   console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
+  /* Done remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.post');
 
@@ -23,10 +24,14 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
+  const href = clickedElement.getAttribute('href');
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const currentArticle = document.querySelector(href);
 
   /* add class 'active' to the correct article */
+  currentArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
