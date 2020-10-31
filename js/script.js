379eb274f8a9e99503ea636292ae2f494ffd0f34
Krzysztof Augustyniak <augustyniak@o2.pl>
2020-10-31 09:51:00 +0100

    JS changes 5-3-5

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!'); 
-  console.log(event)
-  /* remove class 'active' from all article links  */
+  
+  /* Done remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
@@ -11,7 +12,11 @@ function titleClickHandler(event){
   }
   /* add class 'active' to the clicked link */
 
+  clickedElement.classList.add('active')
+  console.log('clickedElement:', clickedElement);
+
   /* remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.post');
 
   for(let activeArticle of activeArticles){
